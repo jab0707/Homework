5d2bcec573b86ceb38c7995510ea2ed3f9637651
jab0707 <jab0707@westminstercollege.edu>
2018-10-20 11:47:25 -0600

    a4

---
+++ IntroToImageProcessing/A4/CS6640_MM.m
@@ -0,0 +1,20 @@
+function M = CS6640_MM(vidObj)
+% CS6640_MM - segments moving objects in video
+% On input:
+%   vidObj (video object obtained by VideoReader): input video
+% On output:
+%   M (Matlab movie): movie of segmented moving objects
+% Call:
+%   vidObj = VideoReader(’../../../video.avi’);
+%   M = CS6640_MM(vidObj);
+% Author:
+% 	Jake Bergquist
+%   Fall 2018
+%   U of U
+
+
+
+
+
+
+end
