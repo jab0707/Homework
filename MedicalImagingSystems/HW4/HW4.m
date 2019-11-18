767fb406a3d82293fca1d7d6be8026653fd2b5e4
jab0707 <jab0707@westminstercollege.edu>
2019-11-17 19:52:26 -0700

    update

--- MedicalImagingSystems/HW4/HW4.m
+++ MedicalImagingSystems/HW4/HW4.m
@@ -12,20 +12,43 @@ X = (D*uair)/(0.873*ulung);
 
 %%
 
-lambdaVals = [10,8,6,4,2,1,0.1,0.05,0.01];
-
-for ind = 1:length(lambdaVals)
-    %Generate new image for each lambda value
-    images(ind).original = gen_im(6,0.2);
-    dist = poissrnd(lambdaVals(ind),256,256);
-    dist = dist/max(dist(:));
-    images(ind).exposed = dist.*images(ind).original;
-    figure(ind); clf();
-    subplot(211);
-    imshow(images(ind).original);
-    title(sprintf('Original for Labmda %d',lambdaVals(ind)))
-    subplot(212);
-    imshow(images(ind).exposed);
-    title(sprintf('Exposed for Labmda %d',lambdaVals(ind)))
+lambdaVals = [10,5,2,1,0.8,0.6,0.4,0.3,0.2,0.1];
+intensities = [0.2,0.4,0.6,0.8];
+%Generate the images for me to look at
+for ind1 = 1:length(intensities)
+    for ind2 = 1:length(lambdaVals)
+        currentIndex = length(lambdaVals)*(ind1-1) + ind2;
+        fprintf(sprintf('index: %d\n',currentIndex))
+        %Generate new image for each lambda value
+        images(currentIndex).original = gen_im(6,intensities(ind1));
+        dist = poissrnd(lambdaVals(ind2),256,256);
+        dist = dist/max(dist(:));
+        images(currentIndex).exposed = dist.*images(currentIndex).original;
+        figure(currentIndex); clf();
+        subplot(211);
+        imshow(images(currentIndex).original);
+        title(sprintf('Original for Labmda %d, intensity %d',lambdaVals(ind2),intensities(ind1)))
+        subplot(212);
+        imshow(images(currentIndex).exposed);
+        title(sprintf('Exposed for Labmda %d, intensity %d',lambdaVals(ind2),intensities(ind1)))
+    end
 end
+save('images.mat','images')
+%%
 
+%Display the images for my user to see
+circleSeen = zeros(length(intensities),length(lambdaVals));
+for ind1 = 1:length(intensities)
+    for ind2 = 1:length(lambdaVals)
+        currPosition = get(figure(1),'position');
+        currentIndex = length(lambdaVals)*(ind1-1) + ind2;
+        figure(1);
+        imshow(images(currentIndex).exposed);
+        title('Do you see a disntict circle?')
+        set(figure(1),'position',currPosition);
+        in = input('Do you see a circle in this image? 1:y 0:n ');
+        circleSeen(ind1,ind2) = in;
+    end
+end
+%%
+save('observerResponse_1.mat','circleSeen')
